58aeaaaf1a5355ed7ae6b322550820938ab95732
wtjian655 <151194034+wtjian655@users.noreply.github.com>
2024-10-25 19:36:44 +0800

    tb_full_adder.v

--- Lab2/1_fulladder/Quartus_prj/sim/tb_full_adder.v
+++ Lab2/1_fulladder/Quartus_prj/sim/tb_full_adder.v
@@ -1,21 +1,5 @@
 `timescale  1ns/1ns
-////////////////////////////////////////////////////////////////////////
-// Author        : EmbedFire
-// Create Date   : 2019/03/13
-// Module Name   : tb_full_adder
-// Project Name  : full_adder
-// Target Devices: Altera EP4CE10F17C8N
-// Tool Versions : Quartus 13.0
-// Description   : 全加器仿真文件
-//
-// Revision      : V1.0
-// Additional Comments:
-// 
-// 实验平台: 野火_征途Pro_FPGA开发板
-// 公司    : http://www.embedfire.com
-// 论坛    : http://www.firebbs.cn
-// 淘宝    : https://fire-stm32.taobao.com
-////////////////////////////////////////////////////////////////////////
+
 
 module  tb_full_adder();
 
@@ -31,23 +15,21 @@ reg             in1;
 reg             in2;
 reg             cin;
 
-//********************************************************************//
-//***************************** Main Code ****************************//
-//********************************************************************//
-//初始化输入信号
+
+
 initial begin
     in1 <= 1'b0;
     in2 <= 1'b0;
     cin <= 1'b0;
 end
 
-//in1:产生输入随机数，模拟加数1的输入情况
-always #10 in1 <= {$random} % 2;    //取模求余数，产生随机数1'b0、1'b1，每隔10ns产生一次随机数
 
-//in2:产生输入随机数，模拟加数2的输入情况
+always #10 in1 <= {$random} % 2;   
+
+
 always #10 in2 <= {$random} % 2;
 
-//cin:产生输入随机数，模拟前级进位的输入情况
+
 always #10 cin <= {$random} % 2;
 
 initial begin
