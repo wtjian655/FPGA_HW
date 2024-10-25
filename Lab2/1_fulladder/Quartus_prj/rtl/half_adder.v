5e2d6761430a932dcd8651da6fef6e7af074db7a
wtjian655 <151194034+wtjian655@users.noreply.github.com>
2024-10-25 19:34:44 +0800

    half_adder.v

--- Lab2/1_fulladder/Quartus_prj/rtl/half_adder.v
+++ Lab2/1_fulladder/Quartus_prj/rtl/half_adder.v
@@ -2,11 +2,11 @@
 
 module  half_adder
 (
-    input   wire    in1 ,   //加数1
-    input   wire    in2 ,   //加数2
+    input   wire    in1 ,   
+    input   wire    in2 ,   
     
-    output  wire    sum ,   //两个数的加和
-    output  wire    cout    //加和后的进位
+    output  wire    sum ,   
+    output  wire    cout    
 );
 
 
